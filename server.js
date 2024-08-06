5b76fbf4a55ad182875f82ae89393a514062fa86
AkshayJos <akshayjoshi1018@gmail.com>
2024-08-06 23:19:01 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(errorController);
 
 app.use(express.static(path.join(__dirname,"./client/build")));
 app.get('*',function(_,res){
-    res.sendFile(path.join(__dirname,"build","./client/build/index.html"),function(error){
+    res.sendFile(path.join(__dirname,"./client/build/index.html"),function(error){
       res.status(500).send(error);
     })
 })
